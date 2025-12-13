a52cf815e0b07724dc339e4d5d546cd91c7a918c
IsaelSousa <isael.sousa@sequor.com.br>
2025-12-13 18:58:45 -0300

    feat: branch

--- src/services/apollo.ts
+++ src/services/apollo.ts
@@ -3,4 +3,4 @@ import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 export const apolloClient = new ApolloClient({
   link: new HttpLink({ uri: "http://192.168.15.10:4000/graphql" }),
   cache: new InMemoryCache(),
-});
+}); 
